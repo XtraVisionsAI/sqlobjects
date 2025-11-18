73af81754087d82b8eee344aaca68005ec539edb
Chen Hao <chenhao@xtravisions.com>
2025-11-18 11:54:11 +0800

    build: implement FHS-compliant package distribution

--- scripts/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Scripts for SQLObjects package management."""
--- setup.py
+++
@@ -1,7 +0,0 @@
-"""Minimal setup.py for compatibility."""
-
-from setuptools import setup
-
-
-# All configuration is in pyproject.toml
-setup()
